c40c5a264ba2099eabbba90cb253d0ec09b13023
Emrikcah <curryandr3@gmail.com>
2020-09-17 09:22:54 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-const iconContainer = document.querySelector('.icon-container');
+const toggleOpen = document.querySelector('[data-open]');
 const showIcons = document.querySelector('.social-icons');
+const toggleClose = document.querySelector('[data-close');
 
-iconContainer.addEventListener('click',()=>{
-   
+
+toggleOpen.addEventListener('click',()=>{
+      console.log('open');
+      showIcons.classList.toggle('fromBottom')
+})
+
+toggleClose.addEventListener('click',()=>{
+      console.log('close');
+
+      showIcons.classList.toggle('fromBottom')
 })
