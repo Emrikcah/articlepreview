ba7268e2d81a2f7aa2401447f3823ad8db78d5bf
Emrikcah <curryandr3@gmail.com>
2020-09-17 07:36:53 -0400

    added js hooks to html

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,15 @@
-const toggleOpen = document.querySelector('.icon-container');
+const toggleOpen = document.querySelector('[data-open]');
 const showIcons = document.querySelector('.social-icons');
-const toggleClose = document.querySelector('.closeBtn');
+const toggleClose = document.querySelector('[data-close');
 
 
 toggleOpen.addEventListener('click',()=>{
+      console.log('open');
       showIcons.classList.toggle('fromBottom')
 })
 
 toggleClose.addEventListener('click',()=>{
+      console.log('close');
+
       showIcons.classList.toggle('fromBottom')
 })
